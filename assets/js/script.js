e12015672cd0f7ab49453fc767339ab27cd5124a
JAmcevoy <>
2023-11-28 18:58:04 +0000

    added all the rule to the game on to a switch and gave each an alert (com wins all so far)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,35 @@
-function runGame();
-
-
-
-function chackPlayerChoice();
-
-
-
-function checkomputerChoice();
-
-
-
-function addPlayerScore() {
-
-    // Gets the current tally of incorrect answers from the DOM and increments it
-
-    let oldScore = parseInt(document.getElementById("win").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-
-}
-
-
-
-function addComputerScore() {
-
-    // Gets the current tally of incorrect answers from the DOM and increments it
-
-    let oldScore = parseInt(document.getElementById("lose").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-
+switch (true) {
+    case computerChoice === playerChoice:
+        alert("It's a Draw, Try again!");
+        break;
+    case computerChoice === scissors && playerChoice === paper:
+        alerts("Scissors cuts Paper");
+        break;
+    case computerChoice === paper && playerChoice === rock:
+        alerts("Paper covers Rock");
+        break;
+    case computerChoice === rock && playerChoice === lizzard:
+        alerts("Rock crushes Lizard");
+        break;
+    case computerChoice === lizzard && playerChoice === spock:
+        alerts("Lizard poisons Spock")
+        break;
+    case computerChoice === spock && playerChoice === scissors:
+        alerts("Spock smashes Scissors");
+        break;
+    case computerChoice === scissors && playerChoice === lizzard:
+        alerts("Scissors decapitates Lizard");
+        break;
+    case computerChoice === lizzard && playerChoice === paper:
+        alerts("Lizard eats Paper");
+        break;
+    case computerChoice === paper && playerChoice === spock:
+        alerts("Paper disproves Spock");
+        break;
+    case computerChoice === spock && playerChoice === rock:
+        alerts("Spock vaporizes Rock");
+        break;
+    case computerChoice === rock && playerChoice === scissors:
+        alerts("Rock crushes Scissors");
+        break;
 }
